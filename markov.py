6a77c5f7aaf52718ae099f84b01447931c2da9bc
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 16:37:28 -0700

    completed bigram dictionary function make_chains()

--- markov.py
+++ markov.py
@@ -5,15 +5,26 @@ def make_chains(input_text):
     """Takes input text as string; returns dictionary of markov chains."""
     # input_text is a whole text file with lines
     # input_text.read() ??
-    # input_text.split(" ")
+    # input_text_in_list = input_text.split(" ")
+    content_list = input_text.read().split()
+    # content_list = [w.rstrip("?.,\n") for w in content_list]
+    bigram_dict = {}
+    
+    for index in range(len(content_list) - 2):
+        bigram_key = (content_list[index], content_list[index + 1])
+        word_after_bigram_key = content_list[index + 2]
+
+        if bigram_key not in bigram_dict:
+            bigram_dict[bigram_key] = []
+      
+        bigram_dict[bigram_key].append(word_after_bigram_key)
+        # list.append(value)
+
+
     # the new list created from words in input_text should be stored to a new variable
-    return {}
+    return bigram_dict
 
-
-
-
-
-
+    
 # def make_text(chains):
 #     """Takes dictionary of markov chains; returns random text."""
 
@@ -35,7 +46,12 @@ def make_chains(input_text):
 
 # print random_text
 
-x = open('text file that we are opening ')
+x = open('green-eggs.txt')
 
-make_chains(x)
+# make_chains(x)
+
+# input_text= """Hello this is natalie and mish.
+# We are in class. 
+# It is super hot today.
+# I want air conditioning. """
 
