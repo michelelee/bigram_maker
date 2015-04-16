5827fa14a344dea17f4e0d9ede724482295999a3
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 13:04:43 -0700

    opened and looked at file, commented out some plans

--- markov.py
+++ markov.py
@@ -1,28 +1,41 @@
-import sys
+# import sys
 
 
-def make_chains(corpus):
+def make_chains(input_text):
     """Takes input text as string; returns dictionary of markov chains."""
-
+    # input_text is a whole text file with lines
+    # input_text.read() ??
+    # input_text.split(" ")
+    # the new list created from words in input_text should be stored to a new variable
     return {}
 
 
-def make_text(chains):
-    """Takes dictionary of markov chains; returns random text."""
 
-    return "Here's some random text."
+
+
+
+# def make_text(chains):
+#     """Takes dictionary of markov chains; returns random text."""
+
+#     return "Here's some random text."
 
 
 # Change this to read input_text from a file, deciding which file should
 # be used by examining the `sys.argv` arguments (if neccessary, see the
 # Python docs for sys.argv)
 
-input_text = "Some text"
+#######################################################
+# input_text = "Some text"
 
-# Get a Markov chain
-chain_dict = make_chains(input_text)
+# # Get a Markov chain
+# chain_dict = make_chains(input_text)
 
-# Produce random text
-random_text = make_text(chain_dict)
+# # Produce random text
+# random_text = make_text(chain_dict)
 
-print random_text
+# print random_text
+
+x = open('text file that we are opening ')
+
+make_chains(x)
+
